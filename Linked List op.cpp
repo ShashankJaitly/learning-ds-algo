bde074ab4ca4e9090d8ea72be0d50762ad07d458
ShashankJaitly <shashank16vasu@gmail.com>
2020-09-04 01:54:38 +0530

    --init stacks

--- Linked List op.cpp
+++ Linked List op.cpp
@@ -1,39 +1,38 @@
 #include <bits/stdc++.h>
 using namespace std;
-struct Node
-{
+struct Node {
     int data;
-    Node *next;
+    Node* next;
     Node(int data)
     {
         this->data = data;
         next = NULL;
     }
 };
-struct LinkedList
-{
-    Node *head;
+struct LinkedList{
+    Node* head;
     LinkedList()
     {
         head = NULL;
     }
     Node *reverse(Node *head)
     {
-        if (head == NULL || head->next == NULL)
+        if(head == NULL || head->next == NULL)
             return head;
         Node *rest = reverse(head->next);
         head->next->next = head;
         head->next = NULL;
         return rest;
     }
+   
     void print()
     {
         Node *temp = head;
-        while (temp != NULL)
+        while(temp!=NULL)
         {
             cout << temp->data << " ";
             temp = temp->next;
-        }
+        }       
     }
     void push(int data)
     {
@@ -46,12 +45,12 @@ struct LinkedList
         unordered_set<int> seen;
         Node *curr = head;
         Node *prev = NULL;
-        while (curr != NULL)
+        while(curr != NULL)
         {
-            if (seen.find(curr->data) != seen.end())
+            if(seen.find(curr->data) != seen.end())
             {
                 prev->next = curr->next;
-                delete (curr);
+                delete(curr);
             }
             else
             {
@@ -69,15 +68,15 @@ struct LinkedList
 
         LinkedList result;
 
-        while (l1_ele != NULL)
+        while(l1_ele != NULL)
         {
             set.insert(l1_ele->data);
             l1_ele = l1_ele->next;
         }
 
-        while (l2_ele != NULL)
+        while(l2_ele != NULL)
         {
-            if (set.find(l2_ele->data) != set.end())
+            if(set.find(l2_ele->data) != set.end())
                 result.push(l2_ele->data);
             l2_ele = l2_ele->next;
         }
@@ -106,18 +105,25 @@ struct LinkedList
         result.print();
         return;
     }
+
 };
 int main()
 {
+    #ifndef ONLINE_JUDGE
+        //for getting input from input.txt
+        freopen("input.txt", "r", stdin);
+        //for writting output to output.txt
+        freopen("output.txt", "w", stdout);
+    #endif
     LinkedList l1, l2;
     int n, m, ele;
     cin >> n >> m;
-    for (int i = 0; i < n; i++)
+    for(int i= 0; i<n; i++)
     {
         cin >> ele;
         l1.push(ele);
     }
-    for (int j = 0; j < m; j++)
+    for(int j = 0; j < m; j++)
     {
         cin >> ele;
         l2.push(ele);
